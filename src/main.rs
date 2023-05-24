d9462ef70e99708c27419de8f5e5005ced6e30a9
alby.giaco <alby.giaco@live.it>
2023-05-24 09:23:21 +0200

    fix chessboard vector

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     }
     struct BoardRow {
         row: Vec<Option<ChessPiece>>
+        //row: [ChessPiece;8]
         //(Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>, Option<ChessPiece>)
     }
     struct Board {
@@ -25,7 +26,7 @@ fn main() {
     impl Board {
         fn new() -> Board {
             Board {
-                boardColumn: (
+                boardColumn: [
                     BoardRow {
                         row: [Some(ChessPiece {name: "Rook".to_string(), position: (0,0)}), Some(ChessPiece {name: "Knight".to_string(), position: (0,1)}), Some(ChessPiece {name: "Bishop".to_string(), position: (0,2)}), Some(ChessPiece {name: "Queen".to_string(), position: (0,3)}), Some(ChessPiece {name: "King".to_string(), position: (0,4)}), Some(ChessPiece {name: "Bishop".to_string(), position: (0,5)}), Some(ChessPiece {name: "Knight".to_string(), position: (0,6)}), Some(ChessPiece {name: "Rook".to_string(), position: (0,7)})].to_vec()
                     },
@@ -33,16 +34,16 @@ fn main() {
                         row: [Some(ChessPiece {name: "Pawn".to_string(), position: (1,0)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,1)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,2)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,3)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,4)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,5)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,6)}), Some(ChessPiece {name: "Pawn".to_string(), position: (1,7)})].to_vec()
                     },
                     BoardRow {
-                        row: [None, None, None, None, None, None, None, None]
+                        row: vec![None, None, None, None, None, None, None, None]
                     },
                     BoardRow {
-                        row: [None, None, None, None, None, None, None, None]
+                        row: [None, None, None, None, None, None, None, None].to_vec()
                     },
                     BoardRow {
-                        row: [None, None, None, None, None, None, None, None]
+                        row: [None, None, None, None, None, None, None, None].to_vec()
                     },
                     BoardRow {
-                        row: [None, None, None, None, None, None, None, None]
+                        row: [None, None, None, None, None, None, None, None].to_vec()
                     },
                     BoardRow {
                         row: [Some(ChessPiece {name: "Pawn".to_string(), position: (6,0)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,1)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,2)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,3)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,4)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,5)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,6)}), Some(ChessPiece {name: "Pawn".to_string(), position: (6,7)})].to_vec()
@@ -51,7 +52,7 @@ fn main() {
                         row: [Some(ChessPiece {name: "Rook".to_string(), position: (7,0)}), Some(ChessPiece {name: "Knight".to_string(), position: (7,1)}), Some(ChessPiece {name: "Bishop".to_string(), position: (7,2)}), Some(ChessPiece {name: "Queen".to_string(), position: (7,3)}), Some(ChessPiece {name: "King".to_string(), position: (7,4)}), Some(ChessPiece {name: "Bishop".to_string(), position: (7,5)}), Some(ChessPiece {name: "Knight".to_string(), position: (7,6)}), Some(ChessPiece {name: "Rook".to_string(), position: (7,7)})].to_vec()
                     
                     }
-                )
+                ]
                 
             }
         }
@@ -61,7 +62,7 @@ fn main() {
     for i in 0..8 {
         for j in 0..8 {
             
-            match board.boardColumn[i]row[j] {
+            match board.boardColumn[i].row[j] {
                 Some(ref piece) => println!("{} at ({},{})", piece.name, piece.position.0, piece.position.1),
                 None => println!("None"),
             }
